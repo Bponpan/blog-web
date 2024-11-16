d3f5b791dd2afcd0d7ae54e8d2d76d5ffc6c6744
ponpanB <151675683+Bponpan@users.noreply.github.com>
2024-11-16 10:28:24 +0700

    fix params == documentId

---
+++ app/blog/[documentId]/page.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+
+const fetchBlog = async (documentId) => {
+  try {
+    const response = await axios.get(
+      `${process.env.STRAIP_BASE_URL}/api/blogs/${documentId}?populate[0]=thumbnail`
+    );
+    return response.data.data;
+  } catch (error) {
+    console.error(
+      "Error fetching blog:",
+      error.response?.data || error.message
+    );
+    return null;
+  }
+};
+
+export default async function Page({ params }) {
+  const { documentId } = params;
+
+  console.log("Fetching blog for documentId:", documentId);
+  const blog = await fetchBlog(documentId);
+
+  if (!blog) {
+    return <div>Error: Blog not found.</div>;
+  }
+
+  return (
+    <div className="flex items-center space-x-6">
+      {blog.thumbnail?.formats?.thumbnail?.url && (
+        <img
+          src={`${process.env.STRAIP_BASE_URL}${blog.thumbnail.formats.thumbnail.url}`}
+          alt={blog.title}
+          className="w-64 h-64 object-cover rounded-md"
+        />
+      )}
+      <div>
+        <h1 className="text-3xl md:text-4xl font-bold text-gray-800">
+          {blog.title}
+        </h1>
+        <p className="text-gray-600 text-xl pt-5">{blog.description}</p>
+        {blog.detail?.map((item, index) =>
+          item.children.map((child, childIndex) =>
+            child.type === "text" ? (
+              <p
+                key={`${index}-${childIndex}`}
+                className="text-gray-600 text-xl pt-5"
+              >
+                {child.text}
+              </p>
+            ) : null
+          )
+        )}
+      </div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -4,34 +4,33 @@ import Link from "next/link";
 const fetchBlogs = async () => {
   try {
     const response = await axios.get(
-      `${process.env.STRAIP_BASE_URL}/api/blogs`
+      `${process.env.STRAIP_BASE_URL}/api/blogs` 
     );
-    // console.log("data", response.data);
     return response.data.data;
   } catch (error) {
-    console.log("error", error);
+    console.error("Error fetching blogs:", error);
     return [];
   }
 };
 
 export default async function Page() {
-  const blog = await fetchBlogs();
-  console.log("blog", blog);
+  const blogs = await fetchBlogs(); 
+  console.log("blogs", blogs);
 
   return (
     <div className="container mx-auto p-8">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {blog.map((blogItem, index) => (
-          <div key={index} className="p-5">
-            <div className="flex flex-col p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow ">
+        {blogs.map((blogItem, index) => (
+          <div key={blogItem.id} className="p-5">
+            <div className="flex flex-col p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow">
               <h3 className="text-2xl font-bold text-purple-800">
-                {blogItem.title}
+                {blogItem.title } 
               </h3>
               <p className="text-gray-600 mt-3 text-sm">
                 {blogItem.description}
               </p>
               <Link
-                href={`blog/${blogItem.id}`}
+                href={`blog/${blogItem.documentId}`} 
                 className="inline-block bg-pink-500 text-white font-medium py-2 px-4 rounded-lg mt-4 text-center hover:bg-pink-600 transition-colors duration-300"
               >
                 See More
